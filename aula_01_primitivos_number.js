c5d23e9d6eba5b5c524089b7e9857fa24d2ef3e9
Alex <alexcodebr@users.noreply.github.com>
2023-02-05 20:45:42 -0500

    initial

---
+++ aula_01_primitivos_number.js
@@ -0,0 +1,14 @@
+//Primitivos: Numeros, strings, booleans
+//1) Tipo "number"
+
+
+const meuNumbero = 3;
+
+const primeiroNumber = 1;
+
+const segundoNumber = 2;
+
+
+const operacaoMatematica = primeiroNumber + segundoNumber;
+
+console.log(`Resultado` + operacaoMatematica)
